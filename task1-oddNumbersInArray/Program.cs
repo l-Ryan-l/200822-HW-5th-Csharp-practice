1185dd302666e00d2c80a955b57a45a3c5c1290a
Ryan <llryanll@yandex.ru>
2022-08-25 21:19:16 +0300

    tasl1 - solved

--- task1-oddNumbersInArray/Program.cs
+++ task1-oddNumbersInArray/Program.cs
@@ -3,3 +3,33 @@
 трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 [345, 897, 568, 234] -> 2 */
+
+int[] FillArray(int limitOfArray)
+{
+    int[] array = new int[limitOfArray];
+    var rnd = new Random();
+    for (int i = 0; i < limitOfArray; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+} 
+
+int CheckEven (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Введите желаемую длину массива: ");
+int limitOfArray = int.Parse(Console.ReadLine()!);
+
+int[] array = FillArray(limitOfArray);
+
+Console.WriteLine("Массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine("Количество четных чисел в массиве: " + CheckEven(array));
