750a5982faf419f8325bb69b477621ac20c5dd5c
Ryan <llryanll@yandex.ru>
2022-08-25 23:16:23 +0300

    task2 - solved

--- task2-summOfOddPositionNumbers/Program.cs
+++ task2-summOfOddPositionNumbers/Program.cs
@@ -3,3 +3,30 @@
 
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0 */
+
+
+void FillArray (int[] array)
+{
+    Console.Write("Введите начальное значение диапазона: ");
+    int begin = int.Parse(Console.ReadLine()!);;
+    Console.Write("Введите конечное значение диапазона: ");
+    int end = int.Parse(Console.ReadLine()!);;
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(begin, end + 1);
+}
+
+int SummOfOddPositions (int[] array)
+{
+    int summ = 0;
+    for (int i = 1; i < array.Length; i+=2)
+        summ = summ + array[i];
+    return summ;
+}
+
+Console.Write("Укажите кол-во элементов в массиве: ");
+int limit = int.Parse(Console.ReadLine()!);
+int[] array = new int[limit];
+
+FillArray(array);
+Console.WriteLine("Массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine($"Сумма элементов массива на нечетных позициях = {SummOfOddPositions(array)}.");
